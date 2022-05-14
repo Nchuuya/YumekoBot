c4e3d4d6290d28a4e54a2d36cb9c56a0ed92aea4
ultroi <86416271+ultroi@users.noreply.github.com>
2022-05-14 19:26:38 +0530

    Update alive.py

--- Yumeko/modules/alive.py
+++ Yumeko/modules/alive.py
@@ -7,18 +7,18 @@ from pyrogram import __version__ as pyrover
 from Yumeko.events import register as MEMEK
 from Yumeko import telethn as tbot
 
-PHOTO = "https://telegra.ph/file/e0040466048302df1a451.mp4"
+PHOTO = "https://telegra.ph/file/b1f139d63252835d7b278.mp4"
 
 @MEMEK(pattern=("/alive"))
 async def awake(event):
   tai = event.sender.first_name
-  YUMEKO = "**Holla I'm Yumeko!** \n\n"
+  YUMEKO = "**Hoi I'm ⚡Pikachu!⚡** \n\n"
   YUMEKO += "×**I'm Working Properly** \n\n"
-  YUMEKO += "×**My Owners : [Tarun • [ᴀc͜͡ɢᴄ]](https://t.me/TheBlacklinen), [亗 ʀʏᴜ](https://t.me/Ryu_God)** \n\n"
+  YUMEKO += "×**My Owners : [RYUK • [✨]](https://t.me/Weeb_lover) \n\n"
   YUMEKO += f"×**Telethon Version : {tlhver}** \n\n"
   YUMEKO += f"×**Pyrogram Version : {pyrover}** \n\n"
   YUMEKO += "**Thanks For Adding Me Here ❤️**"
-  BUTTON = [[Button.url("ʜᴇʟᴘ", "https://t.me/YumekoProBot?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/")]]
+  BUTTON = [[Button.url("ʜᴇʟᴘ", "https://t.me/PikachuRobo_bot?start=help"), Button.url("sᴜᴘᴘᴏʀᴛ", "https://t.me/PikachuHelpSupport")]]
   await tbot.send_file(event.chat_id, PHOTO, caption=YUMEKO,  buttons=BUTTON)
 
 @MEMEK(pattern=("/reload"))
